0fa0e9bcf6a77dd67513897d2b1f9a9909838d48
KushagraKatiyar06 <150945125+KushagraKatiyar06@users.noreply.github.com>
2025-08-01 04:28:06 -0400

    Bug fixed, organized cli

--- Steam_Search/multiFeatureSimilarity.h
+++ Steam_Search/multiFeatureSimilarity.h
@@ -10,30 +10,18 @@
 #include <unordered_map>
 #include <set>
 #include <algorithm>
-
-
 #include "Game.h"
+#include "jaccardsSimilarity.h"
 
-double calculateWeightedJaccard(
-    const std::unordered_map<std::string, int>& tagsA,
-    const std::unordered_map<std::string, int>& tagsB);
+using namespace std;
 
+double jaccardsSimilarityModified(const set<string>& setA, const set<string>& setB);
 
-double calculateJaccard(
-    const std::set<std::string>& setA,
-    const std::set<std::string>& setB
-);
-
-
-double calculateOverallWeightedSimilarity(
-    const Game& gameA,
-    const Game& gameB,
-    double weightTags,
-    double weightPublishers,
-    double weightDevelopers,
-    double weightGenres);
+double calculateOverallWeightedSimilarity(const Game& gameA, const Game& gameB, double weightTags, double weightPublishers, double weightDevelopers, double weightReviewScore, unordered_map<string, Game>& gameData, unordered_map<string, string> decoder);
 
 
 
 
 #endif //MULTIFEATURESIMILARITY_H
+
+// hey
