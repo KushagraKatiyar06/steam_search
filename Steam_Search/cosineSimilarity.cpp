d86863d1f06d37332d5b100fd6a6fe399146b382
KushagraKatiyar06 <playkushagra@gmail.com>
2025-08-01 23:25:56 -0400

    commented code
    commented minhash and cosine

--- Steam_Search/cosineSimilarity.cpp
+++ Steam_Search/cosineSimilarity.cpp
@@ -23,18 +23,26 @@ unordered_map<string, vector<double>> cosineSimilarity::getGameSignatures() {
 void cosineSimilarity::createGameSignatures(const unordered_map<string, Game>& allData) {
     int numTags = indexedTags.size();
 
+
+    //iterate thorugh every game in the dataset nd initialize vectors of size numTags with value 0.
+    //Then every game will be traversed through. Total votes across all tags is computed and in the vector,
+    //each index represents a specific tag. The value at the index is
+    //(number of votes for particular tag) / (total votes for every tag the game has).
     for (auto const& game : allData) {
         string currentName = game.first;
         Game currentGame = game.second;
         vector<double> gameSignature(numTags, 0.0);
 
+
+        //Sums of all the votes for every tag the current game has accumulated
         int totalVotes = 0;
         for (auto tag : currentGame.getTags()) {
             totalVotes += tag.second;
         }
 
+        //Populating the vector with the proportion of votes that a particular tag has relative to all
+        //votes for every tag in the game
         for (auto tag : currentGame.getTags()) {
-
             string currentTag = tag.first;
             int currentVotes = tag.second;
 
@@ -49,6 +57,9 @@ void cosineSimilarity::createGameSignatures(const unordered_map<string, Game>& a
 
 }
 
+//Now two games have their own signatures which is vector full of the weighted voting each tag has. The dot product
+//of both signatures provides the similiarity score after it is normalized using the magnitude of the vector ps: i learned this
+//in linear algebra 2 days before writing this algorithm ;P.
 double cosineSimilarity:: similarity(const string& gameA, const string& gameB) {
     if (gameSignatures.find(gameA) == gameSignatures.end() || gameSignatures.find(gameB) == gameSignatures.end()) {
         return 0.0;
@@ -61,12 +72,16 @@ double cosineSimilarity:: similarity(const string& gameA, const string& gameB) {
     double normA = 0.0;
     double normB = 0.0;
 
+    //Calculates the dot product of the two vectors
     for (int i = 0; i < gameSignatureA.size(); i++) {
         dotProduct += gameSignatureA[i] * gameSignatureB[i];
         normA += pow(gameSignatureA[i], 2);
         normB += pow(gameSignatureB[i], 2);
     }
 
+    //normA squares the signatures of the game to help with normalizing the data
+    //square rooting the previously squared normal value. The normalizing ensures that the score
+    //reamins in between 0 and 1.
     normA = sqrt(normA);
     normB = sqrt(normB);
 
--- Steam_Search/minHash.cpp
+++ Steam_Search/minHash.cpp
@@ -10,15 +10,18 @@
 
 using namespace std;
 
-//Constructor - creates signatures for all games
+//Constructor - creates random hash functions
 minHash:: minHash(int n, const unordered_map<string, int>& tags) {
     this->indexedTags = tags;
-    //create hash combinations
+
+    //This vectoor has a very simple value of 1-numTags, this is before filling it up
+    //with random values based on random hash functions
     vector<int> indicesBeforePermutations;
     for (int i = 0; i < indexedTags.size(); i++) {
         indicesBeforePermutations.push_back(i);
     }
 
+    //Creates N different permutations of the indices. basically shuffling the vector by reordering indices
     srand(time(0));
     for (int i = 0; i < n; i++){
         vector<int> v = indicesBeforePermutations;
@@ -46,13 +49,22 @@ vector<vector<int>> minHash::getHashCombinations() {
 }
 
 //Methods
+
 vector<int> minHash::createSignature(const Game& game) {
     int numCombinations = hashCombinations.size();
     int numTags = indexedTags.size();
+
+    //It is important to initialize the vector this way because the value must be larder than any of the possible hash values.
+    //Later on there will be comparisons done where the smallest index is prioritized, hence the starting vector has the
+    //largest possible value for this particaluar game hashes
     vector<int> signature(numCombinations, numTags + 1);
 
+
+    //Grabs the indices from the basic tag index map for this particular game
     const auto& gameTagsMap = game.getTags();
     vector<int> gameTagIndices;
+
+
     for (const auto& tagData : gameTagsMap) {
         string tagName = tagData.first;
         if (indexedTags.contains(tagName)) {
@@ -64,6 +76,9 @@ vector<int> minHash::createSignature(const Game& game) {
         return signature;
     }
 
+
+    //For the number of hash functions that exist, many different hash functions assigned different values
+    //for each tag, for this alogirhtmn, we find the smallest possible one, which is the minimum hash.
     for  (int i = 0; i < numCombinations; i++) {
 
         for (int tagIndex : gameTagIndices) {
@@ -76,6 +91,10 @@ vector<int> minHash::createSignature(const Game& game) {
     return signature;
 }
 
+//compares the two signatures that should now contain the minimum hash value of each tag previously made
+//works like the regular jaccard algorithm by comparing common values in each spot of the vector by the
+//total hash values between both games. The result is essentially an estimate compared to actually using
+//tags in the jaccards algorithm, due to randomizing hash functions and values.
 double minHash:: miniJaccards(const vector<int>& gameA, const vector<int>& gameB) {
 
     if (gameA.size() != gameB.size() || gameA.empty()) {
